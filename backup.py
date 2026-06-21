d245365995dd62f527c40f0fc68f553b2faf287b
Gorgohn <alisanyildiz0930@icloud.com>
2026-06-21 11:46:15 +0200

    Creating folder in external storage with mkdir and print(target) for control

--- backup.py
+++ backup.py
@@ -1,6 +1,6 @@
 import datetime 
 import pathlib
-import os
+
 
 source_icloud = pathlib.Path(r"C:/Users/amd/iCloudDrive")
 backup_root = pathlib.Path(r"E:/")
@@ -8,4 +8,8 @@ backup_root = pathlib.Path(r"E:/")
 year = datetime.date.today().year
 month = datetime.date.today().month
 
-target = backup_root / str(year) / str(month)
+target = backup_root / str(year) / str(7)
+
+target.mkdir(parents=True, exist_ok=True)
+
+print(target)
