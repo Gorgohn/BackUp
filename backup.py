f11ac9b406bac678856d3602a6d98ab5ebd3168b
Gorgohn <alisanyildiz0930@icloud.com>
2026-06-23 20:02:01 +0200

    Try and except implemented, so if error occurs, the code will not stop and continue with the backup

--- backup.py
+++ backup.py
@@ -12,47 +12,47 @@ month = today.month
 
 target = backup_root / str(year) / str(month)
 
-if not source_icloud.is_dir():
+if not source_icloud.is_dir(): # searching for icloud
     print("Icloud not found")
     sys.exit(1)
 
-if not backup_root.is_dir():
+if not backup_root.is_dir(): # searching for backup
     print("Backup not found")
     sys.exit(1)
 
 print("Icloud found")
 print("Backup found")
 
-def run_backup():
+def run_backup(): # creating function
     copied_files = 0
     skipped_files = 0
     updated_files = 0
+    failed_files = 0
 
-    for new_file in source_icloud.rglob("*"):
-
-        if new_file.is_file():
+    for new_file in source_icloud.rglob("*"): # getting all files
+        if new_file.is_file(): # get path of icloud file and create path in backup
             relative_path = new_file.relative_to(source_icloud)
             destination_file = target / relative_path
-
             destination_file.parent.mkdir(parents=True, exist_ok=True)
-
-            if destination_file.exists():
+            if destination_file.exists(): # setting timestamp for existing files in the backup
                 icloud_file_time = new_file.stat().st_mtime
                 backup_file_time = destination_file.stat().st_mtime
-
-                if icloud_file_time > backup_file_time:
-                    shutil.copy2(new_file, destination_file)
-                    updated_files += 1
-
-                else:
-                    skipped_files += 1
-
+                try:
+                    if icloud_file_time > backup_file_time: # if icloud file is newer than backup file, update the file
+                        shutil.copy2(new_file, destination_file)
+                        updated_files += 1
+                    else: # if backup file is newest, count + 1 skipped file 
+                        skipped_files += 1
+                except:
+                    failed_files += 1 
             else:
-                shutil.copy2(new_file, destination_file)
-                copied_files += 1
+                try: # if file is not in backup, copy to backup
+                    shutil.copy2(new_file, destination_file)
+                    copied_files += 1
+                except:
+                    failed_files += 1
+    return copied_files, updated_files, skipped_files, failed_files # returning values to global
 
-    return copied_files, updated_files, skipped_files
+copied_files, updated_files, skipped_files, failed_files = run_backup() # getting values and running function
 
-copied_files, updated_files, skipped_files = run_backup()
-
-print(f"Backup finished. Copied files: {copied_files}. Updated files: {updated_files}. Skipped files: {skipped_files}.")
+print(f"Backup finished. Copied files: {copied_files}. Updated files: {updated_files}. Skipped files: {skipped_files}. Failed files: {failed_files}")
